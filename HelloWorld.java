69e6d32c554c4e6d120eb5b4cd6efebb4989c43c
kiran <kiran.tonpe280599@gmail.com>
2020-11-21 13:25:23 +0530

    updated_2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
 public static void main(String args[]){
 
-	System.out.println("welcome to jenkins and github ");
+	System.out.println("welcome to jenkins and github and git");
 
 }
 }
